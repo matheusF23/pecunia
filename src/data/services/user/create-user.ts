ef81e9a2f353faa577d0f865b0715ffef76e0b83
Matheus Figueiredo <figueiredo.matheus06@gmail.com>
2022-08-08 21:17:44 -0300

    feat: add crypto repository fro encrypt password

--- src/data/services/user/create-user.ts
+++ src/data/services/user/create-user.ts
@@ -1,17 +1,15 @@
 import { User } from '../../../domain/entity'
 import { CreateUser } from '../../../domain/useCases'
-import { UserRepository } from '../../../data/contracts'
+import { UserRepository, CryptoRepository } from '../../../data/contracts'
 import { UserExistsError } from '../../../domain/errors'
 
 export class CreateUserService implements CreateUser {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(private readonly userRepository: UserRepository, private readonly cryptoRepository: CryptoRepository) {}
 
   async execute(user: User): Promise<void> {
     const userExists = await this.userRepository.findUserByEmail(user.email)
-
     if (userExists) throw new UserExistsError()
-
-    // TODO criptografar a senha
+    user.password = await this.cryptoRepository.encryptPassword(user.password)
     await this.userRepository.createUser(user)
   }
 }
