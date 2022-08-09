9224fdc7d51591f4363660740048a69e3b4d56f6
Matheus Figueiredo <figueiredo.matheus06@gmail.com>
2022-08-08 21:19:04 -0300

    chore: update fakedb

--- src/infra/repositories/fakedb/fake-user.ts
+++ src/infra/repositories/fakedb/fake-user.ts
@@ -1,16 +1,15 @@
-import fs from 'fs'
-
 import { UserModel } from '../../../data/models'
 import { UserRepository } from '../../../data/contracts'
 
+const users: UserModel[] = []
+
 export class FakeUserRepository implements UserRepository {
   async createUser(user: UserModel): Promise<void> {
-    fs.writeFileSync(`${__dirname}/../data-sources/users.txt`, JSON.stringify(user))
+    users.push(user)
+    console.log(users)
   }
 
   async findUserByEmail(email: string): Promise<boolean> {
-    // I will implement when add an real database
-    email
-    return false
+    return users.some(user => user.email === email)
   }
 }
